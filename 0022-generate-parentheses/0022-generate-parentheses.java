08953babe8c854bd14750dd7a929b298f357b156
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-21 19:21:19 +0530

    Time: 2 ms (48.19%), Space: 43.6 MB (19.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,11 +1,6 @@
 class Solution {
-   public List<String> generateParenthesis(int n) {
-       List<String> result = new ArrayList<>();
-       backtrack(result, "", 0, 0, n);
-       return result;
-   }
-   
-   private void backtrack(List<String> result, String current, int open, int close, int n) {
+    public void backtrack(List<String> result, String current, int open, int close, int n)
+    {
        if (current.length() == 2 * n) {
            result.add(current);
            return;
@@ -18,5 +13,10 @@ class Solution {
        if (close < open) {
            backtrack(result, current + ")", open, close + 1, n);
        }
+    }
+    public List<String> generateParenthesis(int n) {
+       List<String> result = new ArrayList<>();
+       backtrack(result, "", 0, 0, n);
+       return result;
    }
 }
