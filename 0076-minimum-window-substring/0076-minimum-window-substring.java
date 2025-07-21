e7e3048d1bcc39a4500c3de24747208b2d290326
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-21 18:12:25 +0530

    Time: 165 ms (8.07%), Space: 45.8 MB (11.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,13 +2,17 @@ class Solution {
     public String minWindow(String s, String t) {
         Map<Character,Integer> details = new HashMap<>();
         int m=s.length(),n=t.length(),i;
+        if (m<n)return"";
+
         for ( i =0;i<n;i++)
         {
             details.put(t.charAt(i),details.getOrDefault(t.charAt(i),0)+1);
         }
+
         int front =0, back =0,size=0,k;
         char ch;
         String temp="",sub="";
+
         while(back<m)
         {
             if (details.containsKey(s.charAt(back)))
@@ -19,28 +23,21 @@ class Solution {
                 size++;
                 details.merge(ch,-1,Integer::sum);
             }
-            // System.out.println(details);
             while(size == n && front<=back)
+            {
+                temp = s.substring(front,back+1);
+                if(sub.length()==0 || sub.length()>temp.length())
+                sub = temp;
+                if (details.containsKey(s.charAt(front)))
                 {
-                    // System.out.println(s.substring(front,back+1)+" "+size);
-                    temp = s.substring(front,back+1);
-                    if(sub.length()==0 || sub.length()>temp.length())
-                    {
-                        sub = temp;
-                        // System.out.println(sub);
-                    }
-                    if (details.containsKey(s.charAt(front)))
-                    {
-                        ch = s.charAt(front);
-                        k=details.get(ch);
-                        if(k>=0)
-                        size--;
-                        details.merge(ch,1,Integer::sum);
-                    }
-                    front++;
-                    // System.out.println(details);
+                    ch = s.charAt(front);
+                    k=details.get(ch);
+                    if(k>=0)
+                    size--;
+                    details.merge(ch,1,Integer::sum);
                 }
-                // System.out.println(s.substring(front,back+1)+" "+size);
+                front++;
+            }
             back++;
         }
         return sub;
