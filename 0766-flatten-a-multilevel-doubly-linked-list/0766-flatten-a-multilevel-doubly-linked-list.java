8d94dd237fce22ae865920d71586659bfb1c4bad
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-24 02:01:34 +0530

    Time: 0 ms (100%), Space: 42.1 MB (23.76%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -11,7 +11,7 @@ class Node {
 class Solution {
     public Node lastnode(Node head)
     {
-        Node curr = head , prevnode=null,temp,nextnode;
+        Node curr = head , prevnode=null,temp;
         while(curr!=null)
         {
             prevnode = curr;
@@ -19,24 +19,38 @@ class Solution {
             curr = curr.next;
             else
             {
-                nextnode = curr.next;
                 temp = lastnode(curr.child);
-                temp.next = nextnode;
+                temp.next = curr.next;
                 curr.next = curr.child;
                 curr.child.prev = curr;
                 curr.child = null;
-                curr = nextnode;
-
+                curr = temp.next;
                 if(curr!=null)
                 curr.prev = temp;
-
                 prevnode = temp;
             }
         }
         return prevnode;
     }
     public Node flatten(Node head) {
-        lastnode(head);
+        Node curr = head,temp=null;
+        while(curr!=null)
+        {
+            if(curr.child == null)
+            curr = curr.next;
+            else
+            {
+                temp = lastnode(curr.child);
+                temp.next = curr.next;
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+                curr = temp.next;
+                
+                if(curr!=null)
+                curr.prev = temp;
+            }
+        }
         return head;
     }
 }
