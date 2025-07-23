6b646250340e0aaa2eb1d7180c2a0d2ab50f9768
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-23 11:19:05 +0530

    Time: 6 ms (5.17%), Space: 42.1 MB (13.56%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -7,47 +7,29 @@ class Solution {
         if (n==1)return s;
         return (s+iter(s,n-1));
     }
-    int start=0,z=1;
+    int start=0;
 
     public String decodeString(String s) {
-        int i=0,l=s.length(),open=0;
-        String str="",num="",temp="",result="";
-        System.out.println(s);
+        int l=s.length();
+        String str="",num="",result="";
         while(start<l)
         {
             if(s.charAt(start)=='[')
             {
                 start++;
-                // for(int y=0;y<z;y++)System.out.print("   ");
-                // z++;
-                str = decodeString(s);
-                // System.out.println(str);
                 if(num=="")num="1";
-                temp = temp + iter(str,Integer.parseInt(num));
+                result = result + iter(decodeString(s),Integer.parseInt(num));
                 num="";
-                // System.out.println("he"+start);
-                z--;
             }
             else if(Character.isDigit(s.charAt(start)))
-            {
-                num = num+s.charAt(start);
-                
-            }
+            num = num+s.charAt(start);
             else if(s.charAt(start)==']')
-            {
-                return(temp);
-            }
+            return(result);
             else
-            {
-                temp+=s.charAt(start);
-                System.out.println(temp);
-            }
-            System.out.println(i);
-            i++;
-            start++;
+            result+=s.charAt(start);
 
+            start++;
         }
-        return temp;
-        
+        return result;
     }
 }
