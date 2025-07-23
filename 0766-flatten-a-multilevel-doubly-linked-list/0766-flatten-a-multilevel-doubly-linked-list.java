e2b080d1b6695d78655a395a2abcd0e963befc9f
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-24 01:48:37 +0530

    Time: 0 ms (100%), Space: 41.6 MB (74.54%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -8,50 +8,102 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node lastnode(Node head)
+//     {
+//         Node curr = head , prevnode=null,temp;
+//         while(curr!=null)
+//         {
+//             prevnode = curr;
+//             if(curr.child == null)
+//             curr = curr.next;
+//             else
+//             {
+//                 temp = lastnode(curr.child);
+//                 temp.next = curr.next;
+//                 curr.next = curr.child;
+//                 curr.child.prev = curr;
+//                 curr.child = null;
+//                 curr = temp.next;
+
+//                 if(curr!=null)
+//                 curr.prev = temp;
+//                 prevnode = temp;
+//             }
+//         }
+//         return prevnode;
+//     }
+//     public Node flatten(Node head) {
+//         Node curr = head,temp=null;
+//         while(curr!=null)
+//         {
+//             if(curr.child == null)
+//             curr = curr.next;
+//             else
+//             {
+//                 temp = lastnode(curr.child);
+//                 temp.next = curr.next;
+//                 curr.next = curr.child;
+//                 curr.child.prev = curr;
+//                 curr.child = null;
+//                 curr = temp.next;
+                
+//                 if(curr!=null)
+//                 curr.prev = temp;
+//             }
+//         }
+//         return head;
+//     }
+// }
+
 class Solution {
-    public Node lastnode(Node head)
-    {
-        Node curr = head , prevnode=null,temp;
-        while(curr!=null)
-        {
-            prevnode = curr;
-            if(curr.child == null)
-            curr = curr.next;
-            else
-            {
-                temp = lastnode(curr.child);
-                temp.next = curr.next;
-                curr.next = curr.child;
-                curr.child.prev = curr;
-                curr.child = null;
-                curr = temp.next;
-                
-                if(curr!=null)
-                curr.prev = temp;
-                prevnode = temp;
-            }
-        }
-        return prevnode;
-    }
     public Node flatten(Node head) {
-        Node curr = head,temp=null;
-        while(curr!=null)
-        {
-            if(curr.child == null)
-            curr = curr.next;
-            else
-            {
-                temp = lastnode(curr.child);
-                temp.next = curr.next;
-                curr.next = curr.child;
-                curr.child.prev = curr;
+        // Start the recursive flattening process
+        flattenList(head);
+        return head;
+    }
+
+    // Helper function that flattens the list starting at 'head'
+    // and returns the last node (tail) of the flattened segment
+    private Node flattenList(Node head) {
+        Node curr = head;
+        Node last = null; // This will track the last node in the flattened list
+
+        while (curr != null) {
+            Node next = curr.next; // Store next node (because we might overwrite it)
+            
+            // If current node has a child, we need to flatten the child list
+            if (curr.child != null) {
+                // Recursively flatten the child list
+                Node childHead = curr.child;
+                Node childTail = flattenList(childHead); // Get the last node of the child list
+
+                // Splice the child list into the main list
+
+                // Connect current node to the head of the child list
+                curr.next = childHead;
+                childHead.prev = curr;
+
+                // Clear the child pointer
                 curr.child = null;
-                curr = temp.next;
-                
-                if(curr!=null)
-                curr.prev = temp;
+
+                // If the next node exists, connect it to the tail of the child list
+                if (next != null) {
+                    childTail.next = next;
+                    next.prev = childTail;
+                }
+
+                // Move current pointer to next (after the inserted child list)
+                last = childTail;
+                curr = next; // Continue from where the child list ended
+            } else {
+                // No child → just move to the next node
+                last = curr;
+                curr = next;
             }
         }
-        return head;
+
+        // Return the last node we reached (tail of the current flattened segment)
+        return last;
     }
 }
