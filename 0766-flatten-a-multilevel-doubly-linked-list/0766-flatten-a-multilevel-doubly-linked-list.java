daee779fa275eab04c6aac75b41ad7d9c3bb2d34
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-24 02:06:47 +0530

    Time: 0 ms (100%), Space: 41.6 MB (87.13%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -7,11 +7,10 @@ class Node {
     public Node child;
 };
 */
-
 class Solution {
     public Node lastnode(Node head)
     {
-        Node curr = head , prevnode=null,temp;
+        Node curr = head , prevnode=null,temp,nextnode;
         while(curr!=null)
         {
             prevnode = curr;
@@ -19,74 +18,24 @@ class Solution {
             curr = curr.next;
             else
             {
+                nextnode = curr.next;
                 temp = lastnode(curr.child);
-                temp.next = curr.next;
+                temp.next = nextnode;
                 curr.next = curr.child;
                 curr.child.prev = curr;
                 curr.child = null;
-                curr = temp.next;
+                curr = nextnode;
+
                 if(curr!=null)
                 curr.prev = temp;
+
                 prevnode = temp;
             }
         }
         return prevnode;
     }
     public Node flatten(Node head) {
-        Node curr = head,temp=null;
-        while(curr!=null)
-        {
-            if(curr.child == null)
-            curr = curr.next;
-            else
-            {
-                temp = lastnode(curr.child);
-                temp.next = curr.next;
-                curr.next = curr.child;
-                curr.child.prev = curr;
-                curr.child = null;
-                curr = temp.next;
-                
-                if(curr!=null)
-                curr.prev = temp;
-            }
-        }
+        lastnode(head);
         return head;
     }
 }
-
-
-// Same Code with less size but still idk why it uses more memory
-
-// class Solution {
-//     public Node lastnode(Node head)
-//     {
-//         Node curr = head , prevnode=null,temp,nextnode;
-//         while(curr!=null)
-//         {
-//             prevnode = curr;
-//             if(curr.child == null)
-//             curr = curr.next;
-//             else
-//             {
-//                 nextnode = curr.next;
-//                 temp = lastnode(curr.child);
-//                 temp.next = nextnode;
-//                 curr.next = curr.child;
-//                 curr.child.prev = curr;
-//                 curr.child = null;
-//                 curr = nextnode;
-
-//                 if(curr!=null)
-//                 curr.prev = temp;
-
-//                 prevnode = temp;
-//             }
-//         }
-//         return prevnode;
-//     }
-//     public Node flatten(Node head) {
-//         lastnode(head);
-//         return head;
-//     }
-// }
