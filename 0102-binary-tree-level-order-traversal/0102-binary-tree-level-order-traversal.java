8281f0ea7ecfc621822735befc4136dd50decc2d
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-25 12:29:33 +0530

    Time: 1 ms (91.25%), Space: 45.1 MB (53.59%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,28 +13,57 @@
  *     }
  * }
  */
-class Solution {
+
+ //***********************Making a new Queue Everytime ********************************************
+ class Solution {
    
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
         if(root == null)return list;
-
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int i,queue_size;
         while(!queue.isEmpty())
         {
-            queue_size = queue.size();
+            Queue<TreeNode> new_queue = new LinkedList<>();
             List<Integer> inner = new ArrayList<>();
-            for(i=0;i<queue_size;i++)
+            while(!queue.isEmpty())
             {
                 TreeNode temp=queue.poll();
-                if(temp.left!=null)queue.offer(temp.left);
-                if(temp.right!=null)queue.offer(temp.right);
+                if(temp.left!=null)new_queue.offer(temp.left);
+                if(temp.right!=null)new_queue.offer(temp.right);
                 inner.add(temp.val);
             }
+            queue = new_queue;
             list.add(inner);
         }
         return list;
     }
 }
+
+
+ // **********************without making any new Queue everytime***********************************
+// class Solution {
+   
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> list = new ArrayList<>();
+//         if(root == null)return list;
+
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.offer(root);
+//         int i,queue_size;
+//         while(!queue.isEmpty())
+//         {
+//             queue_size = queue.size();
+//             List<Integer> inner = new ArrayList<>();
+//             for(i=0;i<queue_size;i++)
+//             {
+//                 TreeNode temp=queue.poll();
+//                 if(temp.left!=null)queue.offer(temp.left);
+//                 if(temp.right!=null)queue.offer(temp.right);
+//                 inner.add(temp.val);
+//             }
+//             list.add(inner);
+//         }
+//         return list;
+//     }
+// }
