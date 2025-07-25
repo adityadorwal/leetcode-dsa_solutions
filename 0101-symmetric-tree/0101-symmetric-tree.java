8c04d50f74bc4d1d91ba8464b0c571d09f0a1c5e
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-26 00:57:51 +0530

    Time: 0 ms (100%), Space: 41.9 MB (40.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,40 +14,40 @@
  * }
  */
 //************************************************Recursive Solution*********************************** */
-// class Solution {
-//     public boolean check(TreeNode leftroot,TreeNode rightroot)
-//     {
-//         if(leftroot == null && rightroot == null)return true;
-//         if((leftroot == null || rightroot == null) || (leftroot.val != rightroot.val))return false;
-//         return(check(leftroot.left,rightroot.right) && check(leftroot.right,rightroot.left));
-//     }
-//     public boolean isSymmetric(TreeNode root) {
-//         return (check(root.left,root.right));
-//     }
-// }
+class Solution {
+    public boolean check(TreeNode leftroot,TreeNode rightroot)
+    {
+        if(leftroot == null && rightroot == null)return true;
+        if((leftroot == null || rightroot == null) || (leftroot.val != rightroot.val))return false;
+        return(check(leftroot.left,rightroot.right) && check(leftroot.right,rightroot.left));
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return (check(root.left,root.right));
+    }
+}
 
 //***************************************************Iterative Solution********************************* */
 
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) return true;
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root.left);
+//         queue.add(root.right);
 
-        while (!queue.isEmpty())
-        {
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            if (t1 == null && t2 == null) continue;
-            if (t1 == null || t2 == null) return false;
-            if (t1.val != t2.val) return false;
-            queue.add(t1.left);
-            queue.add(t2.right);
-            queue.add(t1.right);
-            queue.add(t2.left);
-        }
-        return true;
-    }
-}
+//         while (!queue.isEmpty())
+//         {
+//             TreeNode t1 = queue.poll();
+//             TreeNode t2 = queue.poll();
+//             if (t1 == null && t2 == null) continue;
+//             if (t1 == null || t2 == null) return false;
+//             if (t1.val != t2.val) return false;
+//             queue.add(t1.left);
+//             queue.add(t2.right);
+//             queue.add(t1.right);
+//             queue.add(t2.left);
+//         }
+//         return true;
+//     }
+// }
