806e7faeb212f9eee12f6b3aea5c73755c763be3
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-27 12:03:56 +0530

    Time: 680 ms (5.06%), Space: 55.6 MB (96.84%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n= position.length;
+        if(n==0 || n==1)return n;
+
+        double [][] match = new double[n][2];
+        Stack<Double> stk = new Stack<>();
+
+        for(int i=0;i<n;i++)
+        {
+            match[i][0] = position[i];
+            match[i][1] = (double)(target-position[i])/speed[i];
+        }
+        Arrays.sort(match,(a,b)->Double.compare(b[0],a[0]));
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(match[i][1]);
+            if((stk.isEmpty()) || (match[i][1]>stk.peek()))
+            stk.push(match[i][1]);
+        }
+        return stk.size();
+    }
+}
