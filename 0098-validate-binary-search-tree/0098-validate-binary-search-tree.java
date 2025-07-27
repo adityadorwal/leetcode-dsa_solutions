7b830c7ff6902fd1f49ee196921ffe59bd772e24
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-27 22:57:43 +0530

    Time: 0 ms (100%), Space: 43.7 MB (43.22%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,22 +13,41 @@
  *     }
  * }
  */
+
+ //********************************Iteratibe way************************ */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         Stack<TreeNode> stk = new Stack<>();
+//         TreeNode temp = null;
+//         while(root != null || !stk.isEmpty())
+//         {
+//             while(root!=null)
+//             {
+//                 stk.push(root);
+//                 root= root.left;
+//             }
+//             root = stk.pop();
+//             if (temp!=null && temp.val>=root.val)return false;
+//             temp = root;
+//             root = temp.right;
+//         }
+//         return true;
+//     }
+// }
+
+
+//*************************************Recursive way *************************************** */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        Stack<TreeNode> stk = new Stack<>();
-        TreeNode temp = null;
-        while(root != null || !stk.isEmpty())
-        {
-            while(root!=null)
-            {
-                stk.push(root);
-                root= root.left;
-            }
-            root = stk.pop();
-            if (temp!=null && temp.val>=root.val)return false;
-            temp = root;
-            root = temp.right;
-        }
-        return true;
+    public boolean isValidBST(TreeNode root)
+    {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+
+        return validate(node.left, min, node.val) &&
+               validate(node.right, node.val, max);
     }
 }
