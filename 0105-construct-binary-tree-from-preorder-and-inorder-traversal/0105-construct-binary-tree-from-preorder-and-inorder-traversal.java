337a8aa9a237f9bb1e5d83bebb6e78b0f41d47d6
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-28 13:10:31 +0530

    Time: 1 ms (95.89%), Space: 44.3 MB (77.07%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,62 +14,62 @@
  * }
  */
  //*********************Iterative WAy ********************************* */
-// class Solution {
-//     public TreeNode buildTree(int[] preorder, int[] inorder) {
-//         Stack<TreeNode> stk = new Stack<>();
-//         TreeNode root = new TreeNode(preorder[0]);
-//         stk.push(root);
-//         int k=0;
-
-//         for (int i=1;i<preorder.length;i++)
-//         {
-//             TreeNode node = new TreeNode(preorder[i]);
-//             if(stk.peek().val != inorder[k])
-//             {
-//                 stk.peek().left = node;
-//                 stk.push(node);
-//             }
-//             else
-//             {
-//                 TreeNode temp = null;
-//                 while((!stk.isEmpty()) && stk.peek().val == inorder[k])
-//                 {
-//                     temp = stk.pop();
-//                     k++;
-//                 }
-//                 temp.right=node;
-//                 stk.push(node);
-//             }
-//         }
-//         return root;
-//     }
-// }
-
-//*********************************************REcursive way*********************** */
-
 class Solution {
-    private int preorderIndex = 0;
-    private Map<Integer, Integer> inorderMap = new HashMap<>();
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        for (int i = 0; i < inorder.length; i++) {
-            inorderMap.put(inorder[i], i);
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stk.push(root);
+        int k=0;
+
+        for (int i=1;i<preorder.length;i++)
+        {
+            TreeNode node = new TreeNode(preorder[i]);
+            if(stk.peek().val != inorder[k])
+            {
+                stk.peek().left = node;
+                stk.push(node);
+            }
+            else
+            {
+                TreeNode temp = null;
+                while((!stk.isEmpty()) && stk.peek().val == inorder[k])
+                {
+                    temp = stk.pop();
+                    k++;
+                }
+                temp.right=node;
+                stk.push(node);
+            }
         }
-        return build(preorder, 0, inorder.length - 1);
-    }
-
-    private TreeNode build(int[] preorder, int left, int right)
-    {
-        if (left > right) return null;
-
-        int rootVal = preorder[preorderIndex++];
-        TreeNode root = new TreeNode(rootVal);
-
-        int index = inorderMap.get(rootVal);
-
-        root.left = build(preorder, left, index - 1);
-        root.right = build(preorder, index + 1, right);
-
         return root;
     }
 }
+
+//*********************************************REcursive way*********************** */
+
+// class Solution {
+//     private int preorderIndex = 0;
+//     private Map<Integer, Integer> inorderMap = new HashMap<>();
+
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         for (int i = 0; i < inorder.length; i++) {
+//             inorderMap.put(inorder[i], i);
+//         }
+//         return build(preorder, 0, inorder.length - 1);
+//     }
+
+//     private TreeNode build(int[] preorder, int left, int right)
+//     {
+//         if (left > right) return null;
+
+//         int rootVal = preorder[preorderIndex++];
+//         TreeNode root = new TreeNode(rootVal);
+
+//         int index = inorderMap.get(rootVal);
+
+//         root.left = build(preorder, left, index - 1);
+//         root.right = build(preorder, index + 1, right);
+
+//         return root;
+//     }
+// }
