f9d3955283e62be645411c65df25f918fecbc1ec
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-28 19:02:41 +0530

    Time: 18 ms (2.57%), Space: 47.5 MB (5.21%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution{
+    public int all_path_sum(TreeNode root,ArrayList<Integer> arr)
+    {
+        if(root.left == null && root.right == null) return root.val;
+
+        if(root.left != null && root.right != null)
+        {
+            int left = all_path_sum(root.left,arr);
+            int right = all_path_sum(root.right,arr);
+            int max = Math.max(left,right);
+            arr.add(max);
+            arr.add(max+root.val);
+            arr.add(root.val);
+            arr.add(left+right+root.val);
+            return(Math.max(root.val,max+root.val));
+        }
+        else if(root.left != null)
+        {
+            int left = all_path_sum(root.left,arr);
+            arr.add(left);
+            arr.add(left+root.val);
+            arr.add(root.val);
+            return(Math.max(root.val,left+root.val));
+        }
+        else
+        {
+            int right = all_path_sum(root.right,arr);
+            arr.add(right);
+            arr.add(right+root.val);
+            arr.add(root.val);
+            return(Math.max(root.val,right+root.val));
+        }
+    }
+    public int maxPathSum(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(all_path_sum(root,arr));
+        System.out.println(arr);
+        return (Collections.max(arr));
+
+    }
+}
