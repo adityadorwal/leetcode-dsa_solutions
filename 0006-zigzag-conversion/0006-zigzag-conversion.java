6ec8d174268b69473cdf2738f0d83d0fd700f0dc
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-29 17:26:11 +0530

    Time: 2 ms (99.59%), Space: 44.7 MB (94.18%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,19 +2,21 @@ class Solution {
     public String convert(String s, int numRows) {
         int n = numRows*2-2,l=s.length(),k=0;
         if (n==0)return s;
-        String str="";
+        StringBuilder str = new StringBuilder();
         for(int i=0;i<numRows;i++)
         {
             int c = i;
             while(c<l)
             {
-                str+=s.charAt(c);
+                str.append(s.charAt(c));
                 c+=n;
                 if(i>0 && i<numRows-1 && c-k<l)
-                str+=s.charAt(c-k);
+                {
+                    str.append(s.charAt(c-k));
+                }
             }
             k+=2;
         }
-        return str;
+        return str.toString();
     }
 }
