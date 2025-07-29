c60672a13c920c046c106d50b288d4afcbce92f6
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-07-30 01:16:41 +0530

    Time: 28 ms (14.24%), Space: 43.1 MB (11.84%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -33,22 +33,22 @@ class Solution {
         while(!queue.isEmpty())
         {
             int len = queue.peek().neighbors.size();
+            Node curr_node = queue.poll();
 
             for(int i=0;i<len;i++)
             {
-                Node neigh = queue.peek().neighbors.get(i);
+                Node neigh = curr_node.neighbors.get(i);
 
                 if(map.containsKey(neigh))
-                map.get(queue.peek()).neighbors.add(map.get(neigh));
+                map.get(curr_node).neighbors.add(map.get(neigh));
                 else
                 {
                     Node copied_node = new Node(neigh.val);
-                    map.get(queue.peek()).neighbors.add(copied_node);
+                    map.get(curr_node).neighbors.add(copied_node);
                     queue.offer(neigh);
                     map.put(neigh,copied_node);
                 }
             }
-            queue.poll();
         }
         return map.get(node);
     }
