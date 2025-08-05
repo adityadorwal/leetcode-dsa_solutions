d28f2699d13787d5f4e7ea9b53a86603ecccbc29
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-05 07:30:08 +0530

    Time: 1 ms (84.36%), Space: 41.5 MB (6.58%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,14 +1,27 @@
+// class Solution {
+//     public int mySqrt(int x) {
+//         if(x<2)return x;
+//         long i=0,end = x/2;
+//         while(i<=end)
+//         {
+//             long mid = (i+end)/2;
+//             if(mid*mid == x)return (int)mid;
+//             else if(mid*mid < x)i=mid+1;
+//             else end=mid-1;
+//         }
+//         return (int)i-1;
+//     }
+// }
+
+//*************************************By Newton law************************* */
+
+
 class Solution {
     public int mySqrt(int x) {
-        if(x<2)return x;
-        long i=0,end = x/2;
-        while(i<=end)
-        {
-            long mid = (i+end)/2;
-            if(mid*mid == x)return (int)mid;
-            else if(mid*mid < x)i=mid+1;
-            else end=mid-1;
-        }
-        return (int)i-1;
+        if (x < 2) return x;
+
+        long guess = x;
+        while (guess * guess > x)guess = (guess + x / guess) / 2;
+        return (int) guess;
     }
 }
