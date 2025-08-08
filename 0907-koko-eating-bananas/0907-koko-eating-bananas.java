5a34d5992dd91c9835df950715456446ca4e3d35
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-08 17:06:48 +0530

    Time: 6 ms (99.4%), Space: 45.5 MB (24.21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,36 +1,37 @@
 class Solution {
-
-    public boolean check(int[] piles, int h, int speed) {
-        long hours = 0;
-        for (int pile : piles) {
-            hours += (pile + speed - 1) / speed;  // ceiling division
+    public boolean check(int [] piles, int h,int min)
+    {
+        long c=0;
+        for(int i: piles)
+        {
+            c += (i+min-1)/min;
         }
-        return hours <= h;
+        return c<=h;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = 0;
 
-        // Initialize right to the maximum pile value
-        for (int pile : piles) {
-            right = Math.max(right, pile);
+        int left =0,right=0;
+        for(int i: piles)
+        {
+            left += i;
+            if(right<i)right = i;
         }
 
-        int result = -1;
+        left = Math.max(left/h,1);
+        int result=right;
 
-        // Binary search for the minimum valid speed
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (check(piles, h, mid)) {
-                result = mid;      // valid speed found, try smaller
-                right = mid - 1;
-            } else {
-                left = mid + 1;    // too slow, try faster
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(check(piles,h,mid))
+            {
+                result = mid;
+                right = mid-1;
             }
+            else
+            left = mid+1;
         }
-
+        
         return result;
     }
 }
