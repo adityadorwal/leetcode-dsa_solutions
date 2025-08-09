c4df34359b686960d185ceb48859fdeaa6b57630
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-09 07:18:55 +0530

    Time: 1 ms (99.54%), Space: 63.6 MB (27.16%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,15 +13,42 @@
  *     }
  * }
  */
+
+//************************** DFS Approch ****************************** */
+// class Solution {
+//     public int minDepth(TreeNode root) {
+//         if(root == null)
+//         return 0;
+//         int left = minDepth(root.left);
+//         int right = minDepth(root.right);
+//         if(left == 0 && right == 0)return 1;
+//         if(left == 0)return right+1;
+//         if(right == 0)return left+1;
+//         return (Math.min(left,right)+1);
+//     }
+// }
+
+//*****************************BFS Approch************************************** */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root == null)
-        return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        if(left == 0 && right == 0)return 1;
-        if(left == 0)return right+1;
-        if(right == 0)return left+1;
-        return (Math.min(left,right)+1);
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int depth = 1;
+
+        while (!q.isEmpty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode node = q.poll();
+                if (node.left == null && node.right == null) return depth;
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 }
