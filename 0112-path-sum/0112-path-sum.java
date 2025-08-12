d88d08b949f4755aac9a3c194f40107127cceca0
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-12 09:06:20 +0530

    Time: 0 ms (100%), Space: 43.1 MB (61.02%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,11 +16,7 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null)return false;
-        // System.out.println(root.val+ " " + targetSum);
-        if(root.left == null && root.right == null && targetSum-root.val == 0)return true;
-        boolean l1=false,l2=false;
-        if(root.left!=null)l1 = hasPathSum(root.left,targetSum-root.val);
-        if(root.right!=null)l2 = hasPathSum(root.right,targetSum-root.val);
-        return(l1 || l2);
+        if(root.left == null && root.right == null)return targetSum==root.val;
+        return(hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val));
     }
 }
