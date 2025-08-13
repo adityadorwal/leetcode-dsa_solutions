ca6593b3cd428dc7c8494257104e6ab2c8b8f181
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-13 22:46:41 +0530

    Time: 0 ms (100%), Space: 40.7 MB (28.78%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,11 +1,28 @@
+//****************************************slow but working way************************** 
+// class Solution {
+//     public boolean isPerfectSquare(int num) {
+//         int odd = 1;
+//         while(num>0)
+//         {
+//             num -= odd;
+//             odd += 2;
+//         }
+//         return (num == 0);
+//     }
+// }
+
+
+//************************Binary search way*********************** */
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int odd = 1;
-        while(num>0)
+        long left =0,right=num;
+        while(left <= right)
         {
-            num -= odd;
-            odd += 2;
+            long mid = (left+right)/2;
+            if(mid*mid == num)return true;
+            else if(mid*mid < num)left = mid+1;
+            else right = mid-1;
         }
-        return (num == 0);
+        return false;
     }
 }
