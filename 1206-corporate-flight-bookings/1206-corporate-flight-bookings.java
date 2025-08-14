343c8e06b92d9ec668c47a198d235bd68ff94385
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-14 10:55:02 +0530

    Time: 219 ms (31.53%), Space: 60.9 MB (7.29%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -1,12 +1,34 @@
+// ******************** Brute Force Attack *********************
+// class Solution {
+//     public int[] corpFlightBookings(int[][] bookings, int n) {
+//         int arr[] = new int [n];
+//         for(int []i : bookings)
+//         {
+//             int start = i[0]-1;
+//             int end = i[1];
+//             int seat = i[2];
+//             while(start<end) arr[start++] += seat;
+//         }
+//         return arr;
+//     }
+// }
+
+//*********************************Naive Algorithm *************************** */
+
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int arr[] = new int [n];
-        for(int []i : bookings)
+        int arr[] =  new int[n];
+        for(int i[] : bookings)
         {
-            int start = i[0]-1;
-            int end = i[1];
-            int seat = i[2];
-            while(start<end) arr[start++] += seat;
+            arr[i[0]-1] += i[2];
+            if(i[1]<n) arr[i[1]] -= i[2];
+
+            // for(int j:arr)System.out.print(j + " ");
+            System.out.println();
+        }
+        for(int i=1;i<n;i++)
+        {
+            arr[i] = arr[i]+arr[i-1];
         }
         return arr;
     }
