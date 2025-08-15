f8197cd12067823f9abbb4bf3a3b79b87b274a34
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-15 15:49:25 +0530

    Time: 5 ms (99.23%), Space: 45.7 MB (94.68%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,36 +1,52 @@
-class Solution {
-    public boolean search_row(int [] arr, int target , int left , int right)
-    {
-        while(left<right)
-        {
-            int mid = left + (right - left)/2;
-            if(arr[mid] == target)return true;
-            else if(arr[mid] < target)left = mid+1;
-            else right = mid-1;
-        }
-        if(left<arr.length)return arr[left] == target;
-        return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row_len = matrix[0].length , col_len = matrix.length;
+/*******************************************The brute forc attack *************************** */
+
+// class Solution {
+//     public boolean search_row(int [] arr, int target , int left , int right)
+//     {
+//         while(left<right)
+//         {
+//             int mid = left + (right - left)/2;
+//             if(arr[mid] == target)return true;
+//             else if(arr[mid] < target)left = mid+1;
+//             else right = mid-1;
+//         }
+//         if(left<arr.length)return arr[left] == target;
+//         return false;
+//     }
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int row_len = matrix[0].length , col_len = matrix.length;
         
-        int left=0 , right = row_len;
-        while(left<right)
-        {
-            int mid = left + (right-left)/2;
-            if(matrix[0][mid] == target)return true;
-            else if(matrix[0][mid] < target)left = mid+1;
-            else right = mid-1;
-        }
-        row_len = left;
-        for(int []i : matrix)
-        {
-            if(i[0]<=target)
-            {
-                if(search_row(i, target , 0 , row_len))return true;
-            }
-            else
-            return false;
+//         int left=0 , right = row_len;
+//         while(left<right)
+//         {
+//             int mid = left + (right-left)/2;
+//             if(matrix[0][mid] == target)return true;
+//             else if(matrix[0][mid] < target)left = mid+1;
+//             else right = mid-1;
+//         }
+//         row_len = left;
+//         for(int []i : matrix)
+//         {
+//             if(i[0]<=target)
+//             {
+//                 if(search_row(i, target , 0 , row_len))return true;
+//             }
+//             else
+//             return false;
+//         }
+//         return false;
+//     }
+// }
+
+/**************************************Famous staircase Problem ******************************************/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length,n=matrix[0].length;
+        int i=0,j=n-1;
+        while(i<m && j>=0){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target) i++;
+            else j--;
         }
         return false;
     }
