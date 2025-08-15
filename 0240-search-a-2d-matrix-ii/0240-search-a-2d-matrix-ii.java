336d84bdc3d6c27d1e9d8d54366219eb1bfc8b3b
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-15 15:37:06 +0530

    Time: 6 ms (49.24%), Space: 46.2 MB (38.89%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -22,7 +22,7 @@ class Solution {
             else if(matrix[0][mid] < target)left = mid+1;
             else right = mid-1;
         }
-        row_len = (left < row_len)?left:left;
+        row_len = (left < row_len)?left+1:left;
         for(int []i : matrix)
         {
             if(i[0]<=target)
