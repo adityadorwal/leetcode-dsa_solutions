ad2f413ca58dbe51b50289edddde05c0e28f1bb0
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-15 08:58:59 +0530

    Time: 1 ms (23.87%), Space: 40.8 MB (60.08%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,8 +1,15 @@
+/*****************************Identifying by binary number ********************** */
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         if(((n-1)&n) != 0 || n<=0)return false;
+//         return(Integer.numberOfTrailingZeros(n)%2 == 0);
+//     }
+// }
+
+/*****************************Identifying by log methods*************************** */
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(((n-1)&n) != 0 || n<=0)return false;
-        return(Integer.numberOfTrailingZeros(n)%2 == 0);
-        // double  num = n;
-        // return((Math.log(num)/Math.log(4))%1.0 == 0.0);
+        double  num = n;
+        return((Math.log(num)/Math.log(4))%1.0 == 0.0);
     }
 }
