f37d09a4fafa2bf81a496371b642e49d2cc5dde3
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-16 13:53:06 +0530

    Time: 2 ms (85.46%), Space: 48.4 MB (43.32%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -8,35 +8,77 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ /**************************************Solution Using Stack *************************** */
+// class Solution {
+//     public void reorderList(ListNode head) {
+//         ListNode slow=head,fast=head;
+//         Stack<ListNode> stk = new Stack<>();
+//         stk.push(slow);
+
+//         while(fast.next != null && fast.next.next != null)
+//         {
+//             slow=slow.next;
+//             stk.push(slow);
+//             fast = fast.next.next;
+//         }
+//         slow = slow.next;
+//         ListNode last=null;
+
+//         if(fast.next != null)
+//         {
+//             last = slow;;
+//             slow=slow.next;
+//             last.next = null;
+//         }
+//         while(slow!=null)
+//         {
+//             ListNode prev = stk.pop();
+//             prev.next = last;
+//             last = slow;
+//             slow = slow.next;
+//             last.next = prev;
+//         }
+//         head.next = last;
+//     }    
+// }
+
+
+
+/************************************Solution by reversing the second half and merging it ********************** */
+
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode slow=head,fast=head;
-        Stack<ListNode> stk = new Stack<>();
-        stk.push(slow);
+        if (head == null || head.next == null) return;
 
-        while(fast.next != null && fast.next.next != null)
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null)
         {
-            slow=slow.next;
-            stk.push(slow);
+            slow = slow.next;
             fast = fast.next.next;
         }
-        slow = slow.next;
-        ListNode last=null;
 
-        if(fast.next != null)
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+
+        while (curr != null)
         {
-            last = slow;;
-            slow=slow.next;
-            last.next = null;
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
         }
-        while(slow!=null)
+
+        ListNode first = head, second = prev;
+        while (second != null)
         {
-            ListNode prev = stk.pop();
-            prev.next = last;
-            last = slow;
-            slow = slow.next;
-            last.next = prev;
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+
+            first.next = second;
+            second.next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
         }
-        head.next = last;
-    }    
+    }
 }
