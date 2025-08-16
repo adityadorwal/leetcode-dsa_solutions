3b06d238d7999a3ea5a27a462b54f3eab8c1d718
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-16 11:45:14 +0530

    Time: 2 ms (17.88%), Space: 40.9 MB (23.94%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,15 +1,25 @@
+/*********************************Pure Mathematical Solution *********************************** */
+// class Solution {
+//     public int maximum69Number (int num) {
+//         int x =num,c=0,d=-1;
+//         while(x>0)
+//         {
+//             if(x%10 == 6)d=c;
+//             x=x/10;
+//             c++;
+//         }
+//         if(d==-1)return num;
+//         d=(int)Math.pow(10,d);
+//         num = (num/d + 3)*d + (num%d);
+//         return num;
+//     }
+// }
+
+/**********************************String Methods****************************************************** */
+
 class Solution {
     public int maximum69Number (int num) {
-        int x =num,c=0,d=-1;
-        while(x>0)
-        {
-            if(x%10 == 6)d=c;
-            x=x/10;
-            c++;
-        }
-        if(d==-1)return num;
-        d=(int)Math.pow(10,d);
-        num = (num/d + 3)*d + (num%d);
-        return num;
+        String s = String.valueOf(num);
+        return Integer.parseInt(s.replaceFirst("6", "9"));
     }
 }
