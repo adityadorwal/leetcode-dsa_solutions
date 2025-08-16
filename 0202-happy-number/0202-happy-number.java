3d85f4259b47fa9138c1e3b3c519fbc30e4ac56e
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-16 12:33:48 +0530

    Time: 0 ms (100%), Space: 40.5 MB (93.22%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,20 +1,49 @@
+/*****************************HASHSET Solution for this problem********************* */
+
+// class Solution {
+//     public boolean isHappy(int n) {
+//         HashSet<Integer> num = new HashSet<>();
+//         num.add(n);
+//         while(n!=1)
+//         {
+//             int sum=0;
+//             while(n>0)
+//             {
+//                 int rem = n%10;
+//                 sum+=rem*rem; 
+//                 n=n/10;
+//             }
+//             n=sum;
+//             if(num.contains(n))return false;
+//             num.add(n);
+//         }
+//         return true;
+//     }
+// }
+
+
+/******************************************TWO Pointers approch ****************** */
+
 class Solution {
-    public boolean isHappy(int n) {
-        HashSet<Integer> num = new HashSet<>();
-        num.add(n);
-        while(n!=1)
+    public int square_digit(int n)
+    {
+        int sum=0;
+        while(n>0)
         {
-            int sum=0;
-            while(n>0)
-            {
-                int rem = n%10;
-                sum+=rem*rem; 
-                n=n/10;
-            }
-            n=sum;
-            if(num.contains(n))return false;
-            num.add(n);
+            int rem = n%10;
+            sum+=rem*rem;
+            n=n/10;
         }
-        return true;
+        return (sum);
+    }
+    public boolean isHappy(int n) {
+        int slow = n,fast = n;
+        while(true)
+        {
+            slow = square_digit(slow);
+            fast = square_digit(square_digit(fast));
+            if(slow == 1 && fast == 1)return true;
+            if(slow == fast)return false;
+        }
     }
 }
