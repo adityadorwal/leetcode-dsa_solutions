fb5df6497561bef202ce59162a7c437f85d14944
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-18 12:15:05 +0530

    Time: 11 ms (10.02%), Space: 45.3 MB (5.5%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
@@ -1,12 +1,15 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
         int match = 0 , left=0 ,subcost=0;
+        Queue<Integer> que = new LinkedList<>();
         for(int right=0;right<s.length();right++)
         {
-            subcost += Math.abs(s.charAt(right)-t.charAt(right));
+            int diff = Math.abs(s.charAt(right)-t.charAt(right));
+            que.offer(diff);
+            subcost += diff;
             while(subcost>maxCost)
             {
-                subcost-=Math.abs(s.charAt(left)-t.charAt(left));
+                subcost-=que.poll();
                 left++;
             }
             match = Math.max(match,right-left+1);
