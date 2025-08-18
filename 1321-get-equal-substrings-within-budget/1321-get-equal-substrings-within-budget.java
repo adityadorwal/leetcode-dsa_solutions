d0fb062df210f1b14267d034c9081410b246fba0
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-18 12:18:40 +0530

    Time: 8 ms (64.83%), Space: 43.3 MB (52.06%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
@@ -1,17 +1,38 @@
+/*******************TWO Pointer Method**************************************** */
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        int match = 0 ,subcost=0;
-        Queue<Integer> que = new LinkedList<>();
-        
+        int match = 0 , left=0 ,subcost=0;
         for(int right=0;right<s.length();right++)
         {
-            int diff = Math.abs(s.charAt(right)-t.charAt(right));
-            que.offer(diff);
-            subcost += diff;
-            while(subcost>maxCost)subcost-=que.poll();
+            subcost += Math.abs(s.charAt(right)-t.charAt(right));
+            while(subcost>maxCost)
+            {
+                subcost-=Math.abs(s.charAt(left)-t.charAt(left));
+                left++;
+            }
+            match = Math.max(match,right-left+1);
 
-            match = Math.max(match,que.size());
+    
         }
         return match;
     }
 }
+
+/**********************************Stack WAY ************************ */
+// class Solution {
+//     public int equalSubstring(String s, String t, int maxCost) {
+//         int match = 0 ,subcost=0;
+//         Queue<Integer> que = new LinkedList<>();
+        
+//         for(int right=0;right<s.length();right++)
+//         {
+//             int diff = Math.abs(s.charAt(right)-t.charAt(right));
+//             que.offer(diff);
+//             subcost += diff;
+//             while(subcost>maxCost)subcost-=que.poll();
+
+//             match = Math.max(match,que.size());
+//         }
+//         return match;
+//     }
+// }
