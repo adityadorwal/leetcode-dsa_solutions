3a2f2f7932d7ee9c570178f4c76a324021db02d3
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-18 12:00:19 +0530

    Time: 8 ms (64.83%), Space: 43.2 MB (52.06%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
@@ -1,6 +1,6 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        int match = 0 , left=0 ,subcost=0,len =s.length();
+        int match = 0 , left=0 ,subcost=0;
         for(int right=0;right<s.length();right++)
         {
             subcost += Math.abs(s.charAt(right)-t.charAt(right));
@@ -10,8 +10,6 @@ class Solution {
                 left++;
             }
             match = Math.max(match,right-left+1);
-
-    
         }
         return match;
     }
