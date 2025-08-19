7a9e39a548389de65877643c48a0eeb6de2e7425
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-19 08:51:33 +0530

    Time: 3 ms (94.03%), Space: 61.5 MB (81.82%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,7 +1,6 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        long subarray = 0;
-        int c=0;
+        long subarray = 0,c=0;
         for(int i : nums)
         {
             c=(i==0)?c+1:0;
