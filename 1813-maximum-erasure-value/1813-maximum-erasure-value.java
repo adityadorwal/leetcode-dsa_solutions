5e6e54fea048f4ef075f05706c0006e9cb49f86a
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-19 10:01:44 +0530

    Time: 49 ms (66.94%), Space: 61.1 MB (33.76%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,14 +1,36 @@
+/*******************************Solution using HashMap*********************************** */
+// class Solution {
+//     public int maximumUniqueSubarray(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         int sum=0, max=0,left=0;
+//         for(int i:nums)
+//         {
+//             sum += i;
+//             if(map.containsKey(i))left=Math.max(left,map.get(i));
+//             map.put(i,sum);
+//             max = Math.max(max,sum-left);
+//         }
+//         return max;
+//     }
+// }
+
+/************************************Solution using HashSet********************************** */
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int sum=0, max=0,left=0;
+        int maxresult=0 , currsum=0 , left=0;
+        Set<Integer> set = new HashSet<>();
         for(int i:nums)
         {
-            sum += i;
-            if(map.containsKey(i))left=Math.max(left,map.get(i));
-            map.put(i,sum);
-            max = Math.max(max,sum-left);
+            while(set.contains(i))
+            {
+                set.remove(nums[left]);
+                currsum = currsum-nums[left];
+                left++;
+            }
+            currsum = currsum+i;
+            set.add(i);
+            maxresult = Math.max(maxresult,currsum);
         }
-        return max;
+        return maxresult;
     }
 }
