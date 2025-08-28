d7878f3444e03ec485d5543367b6ac22883e70e0
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-08-28 10:00:52 +0530

    Time: 4 ms (93.58%), Space: 45.4 MB (88.99%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.java
@@ -1,30 +1,73 @@
+/**********************************Using Priority Queue************************************ */
+
+// class Solution {
+//     public void diagonal_sort(int[][]grid , int r, int c)
+//     {
+//         if(r==grid.length-1 || c==grid[0].length-1)return;
+//         PriorityQueue<Integer> pq;
+//         if(r>=0 && c==0)
+//         {
+//             int temp =r;
+//             pq = new PriorityQueue<>((a,b)->b-a);
+//             while(r<grid.length) pq.offer(grid[r++][c++]);
+            
+//             c=0;
+//             r=temp;
+
+//             while(r<grid.length) grid[r++][c++]=pq.poll();
+//             diagonal_sort(grid,temp+1,0);
+//         }
+//         else
+//         {
+//             int temp =c;
+//             pq = new PriorityQueue<>();
+//             while(c<grid[0].length) pq.offer(grid[r++][c++]);
+            
+//             c=temp;
+//             r=0;
+
+//             while(c<grid[0].length) grid[r++][c++]=pq.poll();
+//             diagonal_sort(grid,0,temp+1);
+//         }
+//     }
+//     public int[][] sortMatrix(int[][] grid) {
+//         diagonal_sort(grid,0,0);
+//         diagonal_sort(grid,0,1);
+//         return grid;
+//     }
+// }
+
+/***************************************Using Array******************************* */
+
 class Solution {
     public void diagonal_sort(int[][]grid , int r, int c)
     {
         if(r==grid.length-1 || c==grid[0].length-1)return;
-        PriorityQueue<Integer> pq;
+        ArrayList<Integer> arr;
         if(r>=0 && c==0)
         {
             int temp =r;
-            pq = new PriorityQueue<>((a,b)->b-a);
-            while(r<grid.length) pq.offer(grid[r++][c++]);
+            arr = new ArrayList<>();
+            while(r<grid.length) arr.add(grid[r++][c++]);
             
+            Collections.sort(arr, Collections.reverseOrder());
             c=0;
             r=temp;
 
-            while(r<grid.length) grid[r++][c++]=pq.poll();
+            for(int i:arr)grid[r++][c++]=i;
             diagonal_sort(grid,temp+1,0);
         }
         else
         {
             int temp =c;
-            pq = new PriorityQueue<>();
-            while(c<grid[0].length) pq.offer(grid[r++][c++]);
+            arr = new ArrayList<>();
+            while(c<grid[0].length) arr.add(grid[r++][c++]);
             
+            Collections.sort(arr);
             c=temp;
             r=0;
 
-            while(c<grid[0].length) grid[r++][c++]=pq.poll();
+            for(int i:arr)grid[r++][c++]=i;
             diagonal_sort(grid,0,temp+1);
         }
     }
