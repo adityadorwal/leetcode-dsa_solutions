e412458ce4a5648146cececaa7b58e6f2ff67823
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-09-03 11:17:15 +0530

    Time: 12 ms (95.4%), Space: 45.7 MB (52.9%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -6,22 +6,20 @@ class Solution {
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
 
-        boolean[][] visited = new boolean[n][n];
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[] { 0, 0, 1 });
-        visited[0][0] = true;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0], y = cur[1], dist = cur[2];
 
-            if (x == n - 1 && y == n - 1)
+            if (x == n-1 && y == n-1)
                 return dist;
 
             for (int[] d : dirs) {
                 int nx = x + d[0], ny = y + d[1];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 0 && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
+                if (nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0 && grid[nx][ny]==0) {
+                    grid[nx][ny] = 1;
                     q.offer(new int[] { nx, ny, dist + 1 });
                 }
             }
