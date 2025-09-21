4fb5897f803b8a36acc66e916b816cc82487d42c
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-09-21 20:17:21 +0530

    Time: 5 ms (98.16%), Space: 44.5 MB (46.03%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int []dist = new int [n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for(int i=0;i<=k;i++)
+        {
+            int[] temp = dist.clone();
+            for(int j[]:flights)
+            {
+                int u=j[0],v=j[1],w=j[2];
+                if(dist[u] != Integer.MAX_VALUE && dist[u]+w < temp[v])
+                temp[v] = dist[u]+w;
+            }
+            dist = temp;
+        }
+        return (dist[dst]==Integer.MAX_VALUE)?-1:dist[dst];
+    }
+}
