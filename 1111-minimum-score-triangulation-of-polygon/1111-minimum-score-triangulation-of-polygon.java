b15470d94d77a49509d7df81024a1a6df587a9a7
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-09-29 10:12:50 +0530

    Time: 2 ms (99.07%), Space: 41.3 MB (65.63%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -21,3 +21,29 @@ class Solution {
         return poly(values,0,n-1);
     }
 }
+
+// class Solution {
+//     public int minScoreTriangulation(int[] values) {
+//         int n = values.length;
+//         int[][] dp = new int[n][n];
+
+//         // fill dp for sub-polygons of increasing length
+//         for (int length = 3; length <= n; length++)
+//         {  
+//             for (int i = 0; i <= n - length; i++)
+//             {
+//                 int j = i + length - 1;  // end of sub-polygon
+//                 dp[i][j] = Integer.MAX_VALUE;
+
+//                 // try all possible k between i and j
+//                 for (int k = i + 1; k < j; k++) 
+//                 {
+//                     int cost = dp[i][k] + dp[k][j] + values[i] * values[k] * values[j];
+//                     dp[i][j] = Math.min(dp[i][j], cost);
+//                 }
+//             }
+//         }
+
+//         return dp[0][n - 1];
+//     }
+// }
