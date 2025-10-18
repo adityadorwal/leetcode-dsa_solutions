e793fb4aa8e5edf2968cc286ff65b69dbcc40f1d
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-10-18 10:33:28 +0530

    Time: 19 ms (84.14%), Space: 57.6 MB (96.55%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
@@ -1,18 +1,39 @@
+// class Solution {
+//     public int maxDistinctElements(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         int pre=0;
+//         int j=0;
+//         int z=0;
+//         for(int i:nums)
+//         {
+
+//             int diff = pre-i;
+//             if(z==0 || Math.abs(diff)>k)pre = i-k;
+//             else if(diff<k)pre = i+diff+1;
+//             else continue;
+//             z++;
+//         }
+//         return z;
+//     }
+// }
+
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
         Arrays.sort(nums);
-        int pre=0;
-        int j=0;
-        int z=0;
-        for(int i:nums)
-        {
+        int count = 0;
+        long prev = Long.MIN_VALUE; // track last chosen distinct number
 
-            int diff = pre-i;
-            if(z==0 || Math.abs(diff)>k)pre = i-k;
-            else if(diff<k)pre = i+diff+1;
-            else continue;
-            z++;
+        for (int num : nums) {
+            long left = num - k;
+            long right = num + k;
+
+            // We must pick something > prev and <= right
+            long pick = Math.max(prev + 1, left);
+            if (pick <= right) {
+                count++;
+                prev = pick;
+            }
         }
-        return z;
+        return count;
     }
 }
