13ae98dd6bb341ef9f8e26e79ad0fbcc75774212
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-12-18 13:27:28 +0530

    Time: 5 ms (89.47%), Space: 107.4 MB (5.26%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        long max_result=0;
+        long sum=0;
+        for(int i=0;i<strategy.length;i++)
+        {
+            int num = prices[i];
+            int mul = strategy[i];
+            if(i<k/2)sum+=num*0;
+            else if(i<k)sum+=num*1;
+            else sum += num*mul;
+            max_result += num*mul;
+        }
+        max_result = Math.max(max_result,sum);
+        for(int i=k;i<strategy.length;i++)
+        {
+            sum = sum - (prices[i]*strategy[i]);
+            sum = sum + prices[i];
+
+            sum = sum - (prices[i-(k/2)]);
+
+            sum = sum + (prices[i-k]*strategy[i-k]);
+
+            max_result = Math.max(max_result,sum);
+        }
+        return max_result;
+    }
+}
