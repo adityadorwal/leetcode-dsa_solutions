21c5b75a22366e8ffbf41755e0a4cfbcda197973
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2025-12-24 10:17:56 +0530

    Time: 3 ms (83.68%), Space: 44.2 MB (79.53%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -1,14 +1,18 @@
 class Solution {
     public int minimumBoxes(int[] apple, int[] capacity) {
-        Arrays.sort(capacity);
-        int result;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int i:capacity)pq.offer(i);
         int sum=0;
-        int k = capacity.length-1;
+        int result=0;
         for(int i:apple)
         {
             sum = sum+i;
-            while(sum>0)sum = sum-capacity[k--];
+            while(sum>0)
+            {
+                sum = sum-pq.poll();
+                result++;
+            }
         }
-        return capacity.length-1-k;
+        return result;
     }
 }
